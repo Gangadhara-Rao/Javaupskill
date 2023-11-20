63f30d5a623dbcfd91ff54788e843c95024d2aec
Gangadhara-Rao <86068845+Gangadhara-Rao@users.noreply.github.com>
2023-11-20 19:11:15 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,19 @@
+package Programs;
+
+class ArmstrongNumber{    
+	  public static void main(String[] args)  {    
+	    int sumOfCube=0,a,temp;    
+	    int number=153;    
+	    temp=number;    
+	    while(number>0)    
+	    {    
+	        a=number%10; 
+	        number=number/10;  
+	        sumOfCube=sumOfCube+(a*a*a); //Calculating sum of cubes of digits    
+	    }    
+	    if(temp==sumOfCube)    
+	        System.out.println(temp+" is an armstrong number");     
+	    else    
+	        System.out.println(temp+ "is not an armstrong number");     
+	   }    
+	}  
